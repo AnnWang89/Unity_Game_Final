c7594bd5edfa8fc1a1d3a50d69b130556d761dd9
amy46855 <amy46855@gmail.com>
2022-05-16 14:16:27 +0800

    real test

--- Assets/Script/MainUI_Controller.cs
+++ Assets/Script/MainUI_Controller.cs
@@ -20,7 +20,7 @@ public class MainUI_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //test
+        //test test
     }
 
     private void ClickSetting()
