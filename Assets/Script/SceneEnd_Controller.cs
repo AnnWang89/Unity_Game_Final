4f8f5d023a32cda6700dfb777477136a44643fc5
amy46855 <amy46855@gmail.com>
2022-06-07 14:29:29 +0800

    SceneEnd music

--- Assets/Script/SceneEnd_Controller.cs
+++ Assets/Script/SceneEnd_Controller.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+[RequireComponent(typeof(AudioSource))]
 public class SceneEnd_Controller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Button btnBack;
-    // Start is called before the first frame update
+    public Button btnBack; 
+    public AudioClip bgm;
+    AudioSource audiosource;
     void Start()
     {
         btnBack.onClick.AddListener(ClickBack);
-
+        audiosource = GetComponent<AudioSource>();
+        audiosource.PlayOneShot(bgm);
     }
 
     // Update is called once per frame
@@ -21,7 +24,6 @@ public class SceneEnd_Controller : MonoBehaviour
     }
     private void ClickBack()
     {
-
         Debug.Log("CLICK Back!");
         SceneManager.LoadScene("MainUI");
         Debug.Log("MainUI Load Success");
